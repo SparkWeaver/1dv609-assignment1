32e363b30a0e5cb5068e7608c7a687167bf23013
RuneCodeIO <jl225ps@student.lnu.se>
2023-11-16 14:19:48 +0100

    RED: Add failing test for Dice roll with specified value

--- assignment1/src/test/java/com/dicegame/DiceTest.java
+++ assignment1/src/test/java/com/dicegame/DiceTest.java
@@ -16,4 +16,17 @@ public class DiceTest {
             assertTrue("Dice roll should be between 1 and 6", result >= 1 && result <=6);
         }
     }
+
+    @Test
+    public void returnNumbersBetweenOneAndSpecifiedValueOnDiceRoll() {
+        int specifiedValue = 4;
+        Dice dice = new Dice(specifiedValue);
+
+        int result;
+        for(int i = 0; i < 100; i++) {
+            result = dice.roll();
+            assertTrue(String.format("Dice roll should be between 1 and %d, but was %d", specifiedValue, result), 
+                       result >= 1 && result <= specifiedValue);
+        }
+    }
 }
