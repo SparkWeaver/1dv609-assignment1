d0c84f3740b9f7c61dc5ed3dc8e309ec4166ed2b
RuneCodeIO <jl225ps@student.lnu.se>
2023-11-16 14:36:20 +0100

    REFACTOR: Renamed first method, added string format for message

--- assignment1/src/test/java/com/dicegame/DiceTest.java
+++ assignment1/src/test/java/com/dicegame/DiceTest.java
@@ -7,26 +7,28 @@ import static org.junit.Assert.assertTrue;
 public class DiceTest {
     
     @Test
-    public void returnNumbersBetweenOneAndSixOnDiceRoll() {
-        Dice dice = new Dice();
+    public void returnNumbersBetweenOneAndDefaultSizeOnDiceRoll() {
+        int defaultSize = 6;
+        Dice defaultDice = new Dice();
+        String message = String.format("Dice roll should be between 1 and %d, but was ", defaultSize);
 
         int result;
         for(int i = 0; i < 100; i++) {
-            result = dice.roll();
-            assertTrue("Dice roll should be between 1 and 6", result >= 1 && result <=6);
+            result = defaultDice.roll();
+            assertTrue(message + result, result >= 1 && result <=defaultSize);
         }
     }
 
     @Test
-    public void returnNumbersBetweenOneAndSpecifiedValueOnDiceRoll() {
-        int specifiedValue = 4;
-        Dice dice = new Dice(specifiedValue);
+    public void returnNumbersBetweenOneAndSpecifiedSizeOnDiceRoll() {
+        int specifiedSize = 21;
+        Dice dice = new Dice(specifiedSize);
+        String message = String.format("Dice roll should be between 1 and %d, but was ", specifiedSize);
 
         int result;
         for(int i = 0; i < 100; i++) {
             result = dice.roll();
-            assertTrue(String.format("Dice roll should be between 1 and %d, but was %d", specifiedValue, result), 
-                       result >= 1 && result <= specifiedValue);
+            assertTrue(message + result, result >= 1 && result <= specifiedSize);
         }
     }
 }
