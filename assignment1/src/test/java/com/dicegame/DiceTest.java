0444a86437898f36ab8a724a7486757e41016f67
RuneCodeIO <jl225ps@student.lnu.se>
2023-11-16 13:44:25 +0100

    Checked that testing works as intended

---
+++ assignment1/src/test/java/com/dicegame/DiceTest.java
@@ -0,0 +1,13 @@
+package com.dicegame;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class DiceTest {
+    
+    @Test
+    public void returnNumbersBetweenOneAndSixOnDiceRoll() {
+        assertTrue(true);
+    }
+}
