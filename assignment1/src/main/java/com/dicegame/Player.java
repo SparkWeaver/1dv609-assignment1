af6f6dcf7c0c03f2c7d0056dd83166376aca1e0d
RuneCodeIO <jl225ps@student.lnu.se>
2023-11-17 13:10:26 +0100

    GREEN: Updated Player by implementing getScore method

--- assignment1/src/main/java/com/dicegame/Player.java
+++ assignment1/src/main/java/com/dicegame/Player.java
@@ -3,12 +3,18 @@ package com.dicegame;
 public class Player {
     
     private final String name;
+    private int score;
 
     public Player(String name) {
         this.name = name;
+        score = 0;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getScore() {
+        return score;
     }
 }
