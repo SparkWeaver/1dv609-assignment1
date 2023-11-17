d3371e0b90d01046b4399ccd22786cd55d850d5e
RuneCodeIO <jl225ps@student.lnu.se>
2023-11-17 14:20:07 +0100

    RED: Added failing test for Game object being found

---
+++ assignment1/src/test/java/com/dicegame/GameTest.java
@@ -0,0 +1,28 @@
+package com.dicegame;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Arrays;
+
+public class GameTest {
+
+    private Game game;
+    private Player player1;
+    private Player player2;
+
+    @Before
+    public void setUp() {
+        player1 = new Player("Jon");
+        player2 = new Player("Sara");
+        game = new Game(Arrays.asList(player1, player2));
+    }
+
+    @Test
+    public void gameShouldInitializeCorrectly() {
+        assertNotNull("Game should not be null", game);
+        assertNotNull("Player1 should not be null", player1);
+        assertNotNull("Player2 should not be null", player2);
+    }    
+}
