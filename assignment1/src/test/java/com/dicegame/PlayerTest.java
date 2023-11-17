1cf1b7aed16fa32c8ccb4e91a5bbea25d5099577
RuneCodeIO <jl225ps@student.lnu.se>
2023-11-17 13:32:27 +0100

    RED: Added failing test for Player throwDice with mock Dices

--- assignment1/src/test/java/com/dicegame/PlayerTest.java
+++ assignment1/src/test/java/com/dicegame/PlayerTest.java
@@ -1,6 +1,8 @@
 package com.dicegame;
 
 import org.junit.Test;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.when;
 import static org.junit.Assert.assertEquals;
 
 public class PlayerTest {
@@ -16,5 +18,21 @@ public class PlayerTest {
     public void getScoreReturnZeroWhenPlayerIsInitialized() {
         Player player = new Player("Jon");
         assertEquals(0, player.getScore());
+    }
+
+    @Test
+    public void rollDiceShouldUpdateScoreCorrectly() {
+        Player player = new Player("Jon");
+
+        Dice mockDice1 = Mockito.mock(Dice.class);
+        Dice mockDice2 = Mockito.mock(Dice.class);
+
+        when(mockDice1.roll()).thenReturn(7);
+        when(mockDice2.roll()).thenReturn(7);
+
+        Dice[] mockedDices = { mockDice1, mockDice2 };
+
+        player.throwDice(mockedDices);
+        assertEquals(14, player.getScore());
     }
 }
