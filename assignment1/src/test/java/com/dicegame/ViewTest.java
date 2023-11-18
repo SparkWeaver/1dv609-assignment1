2547ef3ba79b3c34279ec44eb083857a87ee2e9f
RuneCodeIO <jl225ps@student.lnu.se>
2023-11-18 21:33:36 +0100

    RED: Added failure test for printing title and instructions

--- assignment1/src/test/java/com/dicegame/ViewTest.java
+++ assignment1/src/test/java/com/dicegame/ViewTest.java
@@ -3,19 +3,30 @@ package com.dicegame;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 public class ViewTest {
 
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
     private View view;
 
     @Before
     public void setUp() {
         view = new View();
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
     }
 
     @Test
@@ -49,4 +60,16 @@ public class ViewTest {
         String result = view.promptForPlayerName();
         assertEquals(expected, result);
     }
+
+    @Test
+    public void testPrintTitleAndInstructions() {
+        View view = new View();
+        view.printTitleAndInstructions();
+
+        String expectedOutput = 
+            "[ title ]\n" +
+            "[instructions]\n";
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
 }
