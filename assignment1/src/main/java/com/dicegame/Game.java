aaa2cd6bb6f5c7731700a60c177205b6fb3359de
RuneCodeIO <jl225ps@student.lnu.se>
2023-11-18 09:57:35 +0100

    GREEN: Updated game to determin a winner and end the game

--- assignment1/src/main/java/com/dicegame/Game.java
+++ assignment1/src/main/java/com/dicegame/Game.java
@@ -7,11 +7,13 @@ public class Game {
     private List<Player> players;
     private List<Dice> dices;
     private boolean isGameActive;
+    private boolean isGameOver;
 
     public Game(List<Player> players, List<Dice> dices) {
         this.players = players;
         this.dices = dices;
         isGameActive = false;
+        isGameOver = false;
     }
 
     public List<Player> getPlayers() {
@@ -26,12 +28,28 @@ public class Game {
         return isGameActive;
     }
 
+    public boolean isOver() {
+        return isGameOver;
+    }
+
     public void start() {
         isGameActive = true;
 
-        for(Player player : players) {
-            player.throwDice(dices);
+        while(isGameActive) {
+            for(Player player : players) {
+                player.throwDice(dices);
+            }
+            checkForWinner();
         }
+
     }
 
+    private void checkForWinner() {
+        for(Player player : players) {
+            if(player.getScore() >= 21) {
+                isGameActive = false;
+                isGameOver = true;
+            }
+        }
+    }
 }
