8bde87d1e25df444bdf77174e89d2195ea41467f
RuneCodeIO <jl225ps@student.lnu.se>
2023-11-19 12:36:47 +0100

    RED: Added failing method for determine the winner

--- assignment1/src/test/java/com/dicegame/GameTest.java
+++ assignment1/src/test/java/com/dicegame/GameTest.java
@@ -155,6 +155,29 @@ public class GameTest {
         assertTrue(game.isOver());
     }
 
+    @Test
+    public void gameShouldDetermineWinnerWhenGameIsOver() {
+        Player mockP1 = Mockito.mock(Player.class);
+        Player mockP2 = Mockito.mock(Player.class);
+        Player mockP3 = Mockito.mock(Player.class);
+        Player mockP4 = Mockito.mock(Player.class);
+
+        when(mockP1.getState()).thenReturn(State.NON_ACTIVE);
+        when(mockP1.getScore()).thenReturn(24);
+        when(mockP2.getState()).thenReturn(State.NON_ACTIVE);
+        when(mockP2.getScore()).thenReturn(13);
+        when(mockP3.getState()).thenReturn(State.NON_ACTIVE);
+        when(mockP3.getScore()).thenReturn(18);
+        when(mockP4.getState()).thenReturn(State.NON_ACTIVE);
+        when(mockP4.getScore()).thenReturn(19);
+        when(mockP4.getName()).thenReturn("Emma");
+
+        List<Player> players = Arrays.asList(mockP1, mockP2, mockP3, mockP4);
+        game.setPlayers(players);
+
+        assertEquals("Emma", game.determineTheWinner());
+    }
+
     private String createPlayerOutput() {
         return System.lineSeparator() +
                 "Score: 0 Dice roll: 12" + System.lineSeparator() +
